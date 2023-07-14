25565f3549efd988030f4acdc4458668256fecaf
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-07-14 20:11:30 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -201,11 +201,11 @@ document.getElementById('connectWallet').onclick = async () => {
         document.getElementById('mintNFT').onclick = async () => {
             var event = dhNft.methods.mintPrice().call({ from: tdhUsers }).then(function(result) {
                 console.log(result);
-                var mintPrice = JSON.stringify(result.toString());
                 var noOfNft = $("#mintAmount").val();;
                 var cost = (result * noOfNft);
+                var mintPrice = JSON.stringify(cost.toString());
 
-                var mintIt = dhNft.methods.mintNft(noOfNft).send({ from: tdhUsers, value: 250 * noOfNft ether }).then(function(result2){
+                var mintIt = dhNft.methods.mintNft(noOfNft).send({ from: tdhUsers, value: cost }).then(function(result2){
                     console.log(result2);
                     document.getElementById('mintNFT').textContent = "Minted!";
                 });
